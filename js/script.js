5241d7dcec34f22af81a43fd271a96995eadeb88
Thomas Golden <thomasgolden1234@icloud.com>
2020-04-04 18:46:07 -0400

    added style and function

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// on clicking the button, we want the menu items to pop up
+function pet_nose() {
+  //select menu
+  document.getElementById('menu_div').style.display ="inline";
+  document.getElementById("nose").style.marginTop='80px';
+}
